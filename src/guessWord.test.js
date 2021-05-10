feac2b6d319265128d6fed9a17317d9f62700a37
Beatriz Gavira <biagavirete@gmail.com>
2021-05-10 18:34:10 -0300

    feat: create functional tests for the app

---
+++ src/guessWord.test.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { findByTestAttr } from './test/testUtils';
+
+import App from './App';
+
+const setup = (state = {}) => {
+
+  const wrapper = mount(<App />);
+  const inputBox = findByTestAttr(wrapper, 'input-box');
+  
+  inputBox.simulate('change', { target: { value: 'train' } });
+
+  const submitButton = findByTestAttr(wrapper, 'submit-button');
+  submitButton.simulate('click', { preventDefault() {} });
+
+  return wrapper;
+}
+
+describe('no words guessed', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessedWords: []
+    });
+  });
+  test('creates GuessedWords table with one row', () => {
+    const guessedWordRows = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordRows).toHaveLength(1);
+  })  
+});
+
+describe('some words guesses', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessedWords: [{ guessedWord: 'agile', letterMatchCount: 1 }],
+    });
+  });
+  test('adds row to guessedWord table', () => {
+    const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordNodes).toHaveLength(2);
+  })
+});
+
+describe('guess secret word', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessedWords: [{ guessedWord: 'agile', letterMatchCount: 1 }],
+    });
+
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    const mockEvent = { target: { value: 'party' } };
+    inputBox.simulate('click', mockEvent);
+
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    submitButton.simulate('click', { preventDefault() {} });
+  });
+  test('adds row to guessedWords table', () => {
+    const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordsNodes).toHaveLength(2);
+  });
+  test('displays congrats component', () => {
+    const congrats = findByTestAttr(wrapper, 'component-congrats');
+    expect(congrats.text().length).toBeGreaterThan(0);
+  });
+  test('does not display input component contents', () => {
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    expect(inputBox.exists()).toBe(false);
+
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    expect(submitButton.exists()).toBe(false);
+  })
+
+});
