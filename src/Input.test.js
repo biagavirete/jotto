b56a0c75d04adc7dab073196bae5abc7d1c8f488
Beatriz Gavira <biagavirete@gmail.com>
2021-05-10 15:51:37 -0300

    feat: mock useState in input test

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
@@ -20,3 +21,17 @@ test('does not throw warning with expected props', () => {
   checkProps(Input, { secretWord: 'party' });
 });
 
+describe('state controlled input field', () => {
+  test('state updates with value of input box upon change', () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    
+    const mockEvent = { target: { value: 'train' } };
+    inputBox.simulate("change", mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  })
+});
