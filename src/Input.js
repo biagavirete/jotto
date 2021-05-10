d87ee498319782ea0bbb71c50a5b103b86ceb026
Beatriz Gavira <biagavirete@gmail.com>
2021-05-10 16:36:31 -0300

    feat: input and button tests ok

--- src/Input.js
+++ src/Input.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Input({ secretWord }) {
+function Input({ success, secretWord }) {
   const [currentGuess, setCurrentGuess] = React.useState("");
+
+  if (success) {
+    return <div data-test="component-input" />
+  }
 
   return (
     <div data-test="component-input">
@@ -18,6 +22,10 @@ function Input({ secretWord }) {
         <button
           data-test="submit-button"
           className="btn btn-primary mb-2"
+          onClick={(e) => {
+            e.preventDefault();
+            setCurrentGuess("");
+          }}
         >
           Submit
         </button>
