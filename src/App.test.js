19c239644bb14344b170c00e654ec9991a3a520a
Beatriz Gavira <biagavirete@gmail.com>
2021-05-17 18:13:13 -0300

    feat: create module mock and test useEffect

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,37 @@
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from './test/testUtils';
-
 import App from './App';
 
+jest.mock('./actions');
+// eslint-disable-next-line import/first
+import { getSecretWord as mockGetSecretWord } from './actions';
+
 const setup = () => {
-  return shallow(<App />);
+  return mount(<App />);
 }
 
 test('renders without error', () => {
   const wrapper = setup();
   const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent).toHaveLength(1);
+});
+
+describe('get secret word', () => {
+  beforeEach(() => {
+    mockGetSecretWord.mockClear();
+  })
+  test('getSecretWord on app mount', () => {
+    const wrapper = setup();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+  test('getSecretWord does not run on app update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
 })
 
 
